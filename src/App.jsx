36fc4b605effd8a585b5615b8be9cd7fddefe86e
sakibtalukqder <sakibtalukqder07@gmail.com>
2023-11-22 03:03:36 +0600

    Update 0.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // src/App.js
 import React from 'react';
+import './App.css'
 import NavbarComponent from './Components/Navbar/NavbarComponent';
 import Home from './Components/Route/Home'
 import { Route, Routes } from 'react-router-dom';
@@ -8,6 +9,7 @@ import Skill from './Components/Route/Skill';
 import Portfolio from './Components/Route/Portfolio';
 import Contract from './Components/Route/Contract';
 import Education from './Components/Route/Education';
+import Footer from './Components/Shared/Footer';
 
 const App = () => (
   <div className=''>
@@ -20,6 +22,7 @@ const App = () => (
         <Route path='/portfolio' element={<Portfolio />} />
         <Route path='/contract' element={<Contract />} />
       </Routes>
+    <Footer/>
     </NavbarComponent>
   </div>
 );
